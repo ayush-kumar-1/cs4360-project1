2cc53d193676d82da6da93554766a7d32fe37251
Faisal Hossain <faisal.hossain75@gmail.com>
2021-02-25 21:51:11 -0500

    Add AirQuality & Concrete scala files

---
+++ src/main/scala/regression/AirQuality.scala
@@ -0,0 +1,132 @@
+/**
+* Script name: AirQuality.scala
+* Purpose of script: CSCI 4360 Project 1 - Regression Analysis of UCI AirQuality Dataset
+* @author Faisal Hossain
+* Date Created: 02-14-2021
+*/
+
+package regression 
+
+//Scala imports 
+import scala.Console
+import java.io.{File, FileOutputStream}
+
+//other imports 
+import helper.Helper
+
+//ScalaTion imports
+import scalation.analytics.{ANCOVA, Regression, QuadRegression, QuadXRegression, 
+                            CubicRegression, CubicXRegression, LassoRegression, RidgeRegression}
+import scalation.analytics.PredictorMat
+import scalation.analytics.Fit
+import scalation.columnar_db.Relation 
+import scalation.linalgebra.{MatriD, MatrixD, VectorD, VectoD, VectorI}
+import scalation.plot.{Plot, PlotM} 
+import scalation.util.banner
+
+
+object AirQuality extends App {
+
+    val air_quality = Relation.apply("../data/AirQualityUCI/AirQualityUCI.csv", "air_quality", 
+                                domain=null, key = 0, eSep = ";", cPos = null)
+    air_quality.show(5)
+
+    val (x,y) = air_quality.toMatriDD(0 to 10, 11)
+    val ox = new MatrixD(x.dim1, 1, 1.0) ++^ x // x augmented with vector of ones 
+
+    val mu_x = x.mean
+    val mu_y = y.mean
+
+    val x_c = x - mu_x
+    val y_c = y - mu_y
+
+    //Creating all the models
+    val MVR = new Regression(ox,y) 
+    val Quad = new QuadRegression(x,y)
+    val QuadX = new QuadXRegression(x, y) 
+    
+    //val CubicX = new Regression(Helper.reduce_coll(new CubicXRegression(x, y)), y)
+    val cx  = Helper.reduce_coll(new Regression(x, y), threshold = 2)
+    val Cubic = new CubicRegression(cx, y)
+    val CubicX = new CubicXRegression(cx, y)
+    
+    //val Cubic = new CubicRegression(Helper.reduce_coll(new Regression(x, y)), y) 
+    //val CubicX = new CubicXRegression(Helper.reduce_coll(new Regression(x, y)), y) 
+    
+    val Ridge = new RidgeRegression(x_c, y_c) 
+    val Lasso = new LassoRegression(x_c, y_c)
+
+    banner("Correlation Matrix")
+    println(MVR.corrMatrix())
+
+    var (forward, backward, stepwise) = run_model(MVR, "MultipleLinearRegression")
+    var (forwardQ, backwardQ, stepwiseQ) = run_model(Quad, "QuadraticRegression")
+    var (forwardQX, backwardQX, stepwiseQX) = run_model(QuadX, "QuadraticCrossRegression")
+    var (forwardC, backwardC, stepwiseC) = run_model(Cubic, "CubicRegression")
+    var (forwardCX, backwardCX, stepwiseCX) = run_model(CubicX, "CubicCrossRegression")
+
+    
+    banner("Multiple Linear Regression")
+    println(s"Forward: ${forward.fitMap} \n\nBackward: ${backward.fitMap} \n\nStepwise: ${stepwise.analyze().fitMap}")
+    banner("Quadratic Regresssion")
+    println(s"Forward: ${forwardQ.fitMap} \n\nBackward: ${backwardQ.fitMap} \n\nStepwise: ${stepwiseQ.analyze().fitMap}")
+    banner("Quadratic Regression with Cross Terms")
+    println(s"Forward: ${forwardQX.fitMap} \n\nBackward: ${backwardQX.fitMap} \n\nStepwise: ${stepwiseQX.analyze().fitMap}")
+    banner("Cubic Regression")
+    println(s"Forward: ${forwardC.fitMap} \n\nBackward: ${backwardC.fitMap} \n\nStepwise: ${stepwiseC.analyze().fitMap}")
+    banner("Cubic Regression with Cross Terms")
+    println(s"Forward: ${forwardCX.fitMap} \n\nBackward: ${backwardCX.fitMap} \n\nStepwise: ${stepwiseCX.analyze().fitMap}")
+
+    banner("Ridge Regression") 
+
+    Ridge.findLambda
+    println(Ridge.analyze().summary)
+
+    banner("Lasso Regression")
+    println(Lasso.analyze().summary)
+    
+
+    /**
+    * Does forward selection, backward elimination and stepwise regression 
+    * for the given model, then plots and saves the tree models and then 
+    * returns the 3 models generated from the process. 
+    * 
+    * @param model the regression model to be used for all steps 
+    * @param modelName the name of the regression model
+    * @return array of regressions in order of forward, backward, stepwise
+    */
+    def run_model (model: PredictorMat, modelName: String): (PredictorMat, PredictorMat, PredictorMat) = { 
+        var (forReg, forRegMat) = model.forwardSelAll() 
+        var (backReg, backRegMat) = model.backwardElimAll() 
+        var (stepReg, stepRegMat) = model.stepRegressionAll()
+
+        plot_and_save(backRegMat, modelName + "BackElim.png")
+        plot_and_save(forRegMat, modelName + "ForwardSel.png")
+        plot_and_save(stepRegMat, modelName + "StepReg.png")
+
+        val resultArray = new Array[PredictorMat](3)
+        resultArray(0)  = Helper.forwardSelection(model)
+        resultArray(1) = Helper.backwardElimination(model) 
+        resultArray(2) = model.buildModel(model.getX.selectCols(stepReg.toArray))
+
+        return (resultArray(0), resultArray(1), resultArray(2))    
+    } //run_model
+
+    /**
+    * Plots and saves the resutls of the backwards/forwards 
+    * variable selection process. 
+    *
+    * @param regMat the matrix that results from the process 
+    * @param path the path to be saved at 
+    */
+    def plot_and_save (regMat: MatriD, path: String, basePath: String = "plots/AirQuality/Scala/")  = { 
+        val plot = new PlotM(VectorD.range(0, regMat.dim1 - 2), regMat.t, 
+            label = Array[String]("R^2", "adj-R^2", "cvR^2"), 
+            _title = "Quality of Fit vs. Model Complexity", 
+            lines = true)
+        
+        plot.saveImage(basePath + path) 
+
+    } //plot_and_save
+
+} // AirQuality
